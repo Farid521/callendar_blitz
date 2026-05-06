f92c1c6d4aeed6ff91fbeac17ccfc860bd8cb326
farid <faritfir521@gmail.com>
2026-05-06 09:12:36 +0700

    adding command parser

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,37 @@
+use clap::{Parser, Subcommand, command};
 
+#[derive(Parser)]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    Status {
+        status_string: String,
+    },
+    Auth,
+    AddEvent {
+        event_name: String,
+        start: u32,
+        end: u32,
+    }
+}
 
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+
+    match cli.command {
+        Commands::Auth => {
+            println!("auth triggered");
+        },
+        Commands::Status {status_string} => {
+            println!("status trigered, status: {}", status_string)
+        },
+        Commands::AddEvent { event_name, start, end } => {
+            println!("event name: {}, start: {}, end: {}", event_name, start, end)
+        }
+    }
+
 }

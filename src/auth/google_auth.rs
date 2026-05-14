5e0014bea315b99fb3c50b98ea4972b4fd34f69c
farid <faritfir521@gmail.com>
2026-05-14 14:17:38 +0700

    adding get refresh token function even though its wrong

--- src/auth/google_auth.rs
+++ src/auth/google_auth.rs
@@ -1,6 +1,6 @@
 use std::env;
 use clap::builder::Str;
-use reqwest::{self, Client};
+use reqwest::blocking::{Client, get};
 use serde::Serialize;
 
 fn get_env(key: &str) -> Result<String, String> {
@@ -8,23 +8,36 @@ fn get_env(key: &str) -> Result<String, String> {
         .map_err(|_|format!("env with key: {:?} cannot be fetched", key))?;
     Ok(val)
 }
-
 #[derive(Serialize)]
-struct AuthParams {
+struct AuthQuery{
     client_id: String,
+    client_secret: String,
     redirect_uri: String,
     response_type: String,
     scope: String,
-    acces_type: String
+    access_type: String
 }
 
-pub fn get_refresh_token() -> Result<(), String> {
+pub fn get_refresh_token() -> Result<(), Box<dyn std::error::Error>> {
     let client = Client::new();
-
-    let client_id = get_env("CLIENT_ID")?;
     
-    let res = client.get("https://accounts.google.com/o/oauth2/v2/auth")
-        .send();
-
+    let auth_query = AuthQuery {
+        client_id: get_env("CLIENT_ID")?,
+        client_secret: get_env("CLIENT_SECRET")?,
+        redirect_uri: get_env("REDIRECT_URI")?,
+        response_type: String::from("code"),
+        scope: get_env("SCOPE")?,
+        access_type: String::from("offline")
+    };
+    
+    let resp = client.get("https://accounts.google.com/o/oauth2/v2/auth")
+        .query(&auth_query)
+        .send()?;
     Ok(())
 } 
+
+pub fn open_browser_auth() -> Result<(), Box<dyn std::error::Error>>{
+    
+    
+    Ok(())
+}
